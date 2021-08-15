64b04e8bd1782b0fa77b550147ec535a10cabdbf
yparikh8036 <yash.150410107061@gmail.com>
2021-08-14 20:27:13 -0400

    basic done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component, VERSION } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name: string;
+
+  changeName(name: string) {
+    this.name = name;
+  }
 }
